ec29e2e2bcdaf5f46538b2562d4886ab6d90f66d
Nick Sclater <nicksclater@icloud.com>
2020-04-22 18:53:16 +0100

    upload of file

---
+++ web_scraper.py
@@ -0,0 +1,29 @@
+## simple web scraper
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+class Scraper:
+	def __init__(self, site):
+		self.site = site
+
+	def scrape(self):
+		r = urllib.request.urlopen(self.site)
+		html = r.read()
+		parser = 'html.parser'
+		sp = BeautifulSoup(html, parser)
+
+		for tag in sp.findAll('a'):
+			url = tag.get('href')
+			if url is None:
+				continue
+				if 'html'in url:
+					print('\n' + url)
+
+news = 'https://www.news.google.com'
+
+Scraper(news).scrape()
+
+
+
+
