fb1bc52c666571d126571f9816817c4b0a24ad7c
Nick Sclater <nicksclater@icloud.com>
2020-04-24 11:10:53 +0100

    xml

--- basic_ws.py
+++ basic_ws.py
@@ -4,59 +4,73 @@ import bs4 as bs
 import urllib.request
 import pandas as pd
 
-source = 'https://pythonprogramming.net/parsememcparseface/'
+# source = 'https://pythonprogramming.net/parsememcparseface/'
 
-sauce = urllib.request.urlopen(source).read()
-# print(sauce)
+# sauce = urllib.request.urlopen(source).read()
+# # print(sauce)
 
-print('\n')
+# print('\n')
 
-# soup = bs.BeautifulSoup(sauce, 'html.parser') # or 'lxml'
-# for url in soup.find_all('a'):
-#   print(url.get('href'))
+# # soup = bs.BeautifulSoup(sauce, 'html.parser') # or 'lxml'
+# # for url in soup.find_all('a'):
+# #   print(url.get('href'))
 
-soup = bs.BeautifulSoup(sauce,'lxml')
+# soup = bs.BeautifulSoup(sauce,'lxml')
 
-nav = soup.nav
-body = soup.body
-para = soup.find_all('p')
-# para = soup.p
-# print(para)
+# nav = soup.nav
+# body = soup.body
+# para = soup.find_all('p')
+# # para = soup.p
+# # print(para)
 
 
-sections = [nav, body]
+# sections = [nav, body]
 
-# for section in sections:
-#   print('------------')
-#   for url in section.find_all('a'): # hyperlink
-#     print(url.get('href'))
-#   print('\n')
+# # for section in sections:
+# #   print('------------')
+# #   for url in section.find_all('a'): # hyperlink
+# #     print(url.get('href'))
+# #   print('\n')
 
-table = soup.table
+# table = soup.table
 
-table_rows = table.find_all('tr') # table row
+# table_rows = table.find_all('tr') # table row
 
-# print(table_rows)
+# # print(table_rows)
 
-for tr in table_rows:
-  td = tr.find_all('td') # table data
-  td = [i.text for i in td]
-  print(td)
+# for tr in table_rows:
+#   td = tr.find_all('td') # table data
+#   td = [i.text for i in td]
+#   print(td)
 
-print('\n')
-# using Pandas
+# print('\n')
+# # using Pandas
 
-dfs  = pd.read_html(source, header=0)
+# dfs  = pd.read_html(source, header=0)
 
-print(dfs) # returns a list of a dataframe
-print('\n')
+# print(dfs) # returns a list of a dataframe
+# print('\n')
 
-for df in dfs:
-  print(df)
+# for df in dfs:
+#   print(df)
 
-print('\n')
+# print('\n')
 
-print(dfs[0].loc[0,:].values)
+# print(dfs[0].loc[0,:].values)
+
+## site maps
+
+source1 = 'https://pythonprogramming.net/sitemap.xml'
+source2 = 'https://www.bbc.co.uk/sitemaps/https-index-uk-news.xml'
+
+sauce = urllib.request.urlopen(source2).read()
+
+soup = bs.BeautifulSoup(sauce, 'xml')
+
+for url in soup.find_all('loc'):
+  print(url.text)
+
+
 
 
 
