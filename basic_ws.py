3581a390bebda5ec8afcbe68efaa9f26feb04304
Nick Sclater <nicksclater@icloud.com>
2020-04-24 10:37:58 +0100

    pandas

--- basic_ws.py
+++ basic_ws.py
@@ -2,8 +2,11 @@
 
 import bs4 as bs
 import urllib.request
+import pandas as pd
 
-sauce = urllib.request.urlopen('https://bbc.co.uk').read()
+source = 'https://pythonprogramming.net/parsememcparseface/'
+
+sauce = urllib.request.urlopen(source).read()
 # print(sauce)
 
 print('\n')
@@ -14,26 +17,46 @@ print('\n')
 
 soup = bs.BeautifulSoup(sauce,'lxml')
 
-
 nav = soup.nav
 body = soup.body
-para = soup.a
-
-print(soup.nav.prettify())
-
+para = soup.find_all('p')
+# para = soup.p
+# print(para)
 
 
 sections = [nav, body]
 
 # for section in sections:
 #   print('------------')
-#   for url in section.find_all('a'):
+#   for url in section.find_all('a'): # hyperlink
 #     print(url.get('href'))
 #   print('\n')
 
+table = soup.table
 
+table_rows = table.find_all('tr') # table row
 
+# print(table_rows)
 
+for tr in table_rows:
+  td = tr.find_all('td') # table data
+  td = [i.text for i in td]
+  print(td)
+
+print('\n')
+# using Pandas
+
+dfs  = pd.read_html(source, header=0)
+
+print(dfs) # returns a list of a dataframe
+print('\n')
+
+for df in dfs:
+  print(df)
+
+print('\n')
+
+print(dfs[0].loc[0,:].values)
 
 
 
