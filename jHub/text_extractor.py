f7a71959484b77b20cec98cb545fc9ff54a825a6
Nick Sclater <nicksclater@icloud.com>
2020-07-16 15:32:04 +0100

    text extract added

---
+++ jHub/text_extractor.py
@@ -0,0 +1,37 @@
+
+
+import nltk
+import json
+
+def extract_entities(string):
+
+    tagger = nltk.pos_tag(string.split())
+    chucker = nltk.ne_chunk(tagger)
+
+    tmp = {'people':[], 'places':[], 'organisation':[]}
+
+    for i in chucker:
+        if isinstance(i, nltk.tree.Tree):
+            if i.label() == 'PERSON':
+                tmp['people'] = i[0][0]
+            elif i.label() == 'GPE':
+                tmp['places'] = i[0][0]
+            elif i.label() == 'ORGANIZATION':
+                tmp['organisation'] = i[0][0]
+
+    entities_json = json.dumps(tmp, indent=4)
+
+    return entities_json
+
+
+
+test1 = 'My name is Charlotte'
+test2 = "In live in Heighington"
+test3 = 'I work for the Council'
+
+test_cases = [test1, test2, test3]
+
+for test in test_cases:
+    res = extract_entities(test)
+    print(res)
+
